c6323050bd47e4e05978ded77f436d1e59be628b
Michael Mata <michaelvmata@gmail.com>
2019-09-08 17:23:14 -0700

    Lex more keywords and operators
    keywords: true, false, if, else, return
    operators: !-*/<> == !=

--- token/token.go
+++ token/token.go
@@ -24,6 +24,22 @@ const (
 	ASSIGN = "="
 	// PLUS is a plus operator
 	PLUS = "+"
+	// MINUS is a minus operator
+	MINUS = "-"
+	// BANG is a bang operator
+	BANG = "!"
+	// ASTERIK is an asterik operator
+	ASTERIK = "*"
+	// SLASH is a slash operator
+	SLASH = "/"
+	// LT is a less than operator
+	LT = "<"
+	// GT is a greater than operator
+	GT = ">"
+	// EQ is an equality operator
+	EQ = "=="
+	// NEQ is a non equality operator
+	NEQ = "!="
 
 	// COMMA is a comma delimiter
 	COMMA = ","
@@ -43,4 +59,32 @@ const (
 	FUNCTION = "FUNCTION"
 	// LET is a let keyword
 	LET = "LET"
+	// TRUE is a true keyword
+	TRUE = "TRUE"
+	// FALSE is a false keyword
+	FALSE = "FALSE"
+	// IF is a if keyword
+	IF = "IF"
+	// ELSE is a else keyword
+	ELSE = "ELSE"
+	// RETURN is a return keyword
+	RETURN = "RETURN"
 )
+
+var keywords = map[string]Type{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+// LookupIdent lookups up keyword of identifier if exists
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
